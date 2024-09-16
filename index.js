57865e9a5af335f4554194e2bc2732589859cc98
Z A I I K Y <156815062+quangminh1341@users.noreply.github.com>
2024-09-17 03:34:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["Đang chơi FiveM", "Cùng lắng nghe Alymusic"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
